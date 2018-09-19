93f7130ace86e657fec00d31f588f7d110bc77c5
Russ <rep4975@rit.edu>
2018-09-19 09:42:33 -0400

    main_toolbar now inflated and interactable

--- app/src/main/java/com/russperlow/myagenda/MainActivity.java
+++ app/src/main/java/com/russperlow/myagenda/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +27,30 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.frame_layout, MainFragment.newInstance());
         transaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        // Inflate the custom toolbar we created
+        getMenuInflater().inflate(R.menu.main_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+
+        int id = menuItem.getItemId();
+
+        switch (id){
+            case R.id.toolbar_help_button:
+                Toast.makeText(this, "Help Button Clicked", Toast.LENGTH_SHORT);
+                return true;
+            case R.id.toolbar_settings_gear:
+                Toast.makeText(this, "Settings Button Clicked", Toast.LENGTH_SHORT);
+                return true;
+            default:
+                return super.onOptionsItemSelected(menuItem);
+        }
     }
 
     // Display calendar for user to choose due date from
